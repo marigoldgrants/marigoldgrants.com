37be2cfb9b30bf5855226ed951ce4b596588f98e
Tate Carsson <tatecarson@pm.me>
2026-06-09 13:49:43 -0500

    Fix Decap home features preview registration

--- static/admin/preview-templates.js
+++ static/admin/preview-templates.js
@@ -1,14 +1,13 @@
 (function () {
   const CMS = window.CMS;
-  const React = window.React;
+  const createClass = window.createClass;
+  const h = window.h;
 
-  if (!CMS || !React) {
+  if (!CMS || !createClass || !h) {
     return;
   }
 
   CMS.registerPreviewStyle("/admin/preview.css");
-
-  const h = React.createElement;
 
   function toJS(value) {
     if (!value) return value;
@@ -22,58 +21,66 @@
     return image.startsWith("/") ? image : "/" + image.replace(/^\.?\//, "");
   }
 
-  function HomeFeaturesPreview(props) {
-    const data = toJS(props.entry.get("data")) || {};
-    const features = Array.isArray(data.features) ? data.features : [];
+  const HomeFeaturesPreview = createClass({
+    render: function () {
+      const data = toJS(this.props.entry.get("data")) || {};
+      const features = Array.isArray(data.features) ? data.features : [];
+      const getAsset = this.props.getAsset;
 
-    return h(
-      "div",
-      { className: "decap-home-features-preview" },
-      h(
+      return h(
         "div",
-        { className: "strip strip-grey" },
+        { className: "decap-home-features-preview" },
         h(
           "div",
-          { className: "container pt-6 pb-6 pt-md-7 pb-md-10" },
+          { className: "strip strip-grey" },
           h(
             "div",
-            { className: "row justify-content-center" },
-            features.map(function (feature, index) {
-              const title = feature && feature.title ? feature.title : "Untitled";
-              const description = feature && feature.description ? feature.description : "";
-              const image = normalizeImagePath(feature && feature.image);
+            { className: "container pt-6 pb-6 pt-md-7 pb-md-10" },
+            h(
+              "div",
+              { className: "row justify-content-center" },
+              features.map(function (feature, index) {
+                const title = feature && feature.title ? feature.title : "Untitled";
+                const description = feature && feature.description ? feature.description : "";
+                const imagePath = normalizeImagePath(feature && feature.image);
+                const imageAsset = imagePath && getAsset ? getAsset(imagePath) : null;
+                const imageSrc =
+                  imageAsset && typeof imageAsset.toString === "function"
+                    ? imageAsset.toString()
+                    : imagePath;
 
-              return h(
-                "div",
-                { className: "col-12 col-md-6 col-lg-4 mb-2", key: title + "-" + index },
-                h(
+                return h(
                   "div",
-                  { className: "feature" },
-                  image
-                    ? h(
-                        "div",
-                        { className: "feature-image" },
-                        h("img", { alt: title + " icon", src: image })
-                      )
-                    : null,
-                  h(
-                    "a",
-                    { href: "/services/", onClick: function (event) { event.preventDefault(); } },
-                    h("h2", { className: "feature-title" }, title)
-                  ),
+                  { className: "col-12 col-md-6 col-lg-4 mb-2", key: title + "-" + index },
                   h(
                     "div",
-                    { className: "feature-content" },
-                    h("p", null, description)
+                    { className: "feature" },
+                    imageSrc
+                      ? h(
+                          "div",
+                          { className: "feature-image" },
+                          h("img", { alt: title + " icon", src: imageSrc })
+                        )
+                      : null,
+                    h(
+                      "a",
+                      { href: "/services/", onClick: function (event) { event.preventDefault(); } },
+                      h("h2", { className: "feature-title" }, title)
+                    ),
+                    h(
+                      "div",
+                      { className: "feature-content" },
+                      h("p", null, description)
+                    )
                   )
-                )
-              );
-            })
+                );
+              })
+            )
           )
         )
-      )
-    );
-  }
+      );
+    },
+  });
 
   CMS.registerPreviewTemplate("home_features", HomeFeaturesPreview);
 })();
